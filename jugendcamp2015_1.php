89b5b580c845b9219ca2fd7a578a2bfab40055c6
nechoj <nechoj@googlemail.com>
2015-02-11 18:59:28 +0000

    Jugend-Camp-Dateien hinzugefügt

---
+++ jugendcamp2015_1.php
@@ -0,0 +1,157 @@
+<td width="600" align="left" valign="top" bgcolor="#FFFFFF" id="nurlinks">
+      
+	<div style="position:relative; top:-20px; left:0px;  z-index:3 ;width:150px">
+	<table width="600" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td colspan="5" id="contentTable"> 
+            <div align="left">Jugend-Camp 2015</div>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+		<tr id="contentFont">
+          
+        </tr>
+
+
+		<tr id="contentFont">
+          <td width="91" id="contentFont"><div align="left"><img src="bilder/jugendcamp_logo_k.png" width="100" height="100"></div></td>    
+      <td width="355" id="contentFont"><div align="center"><font size="+2"><strong>
+  Jugend-Camp 2015</strong></font></div></td>
+	  <td width="91" id="contentFont"><div align="left"><img src="bilder/jugendcamp_logo_k.png" width="100" height="100"></div></td>
+        </tr>
+
+<!--         
+     <tr>
+	 <td id="contentfont" colspan="3" style= "text-align:left;"><strong>* <span class="Stil21">Liebe Jugend-Camp-Teilnehmer:</span><span class="Stil21"></span></strong>
+     <p align="center"><a href="index.php?id=20"><img src="bilder/shop/cd02.jpg" width="550" border="0" /></a></p>
+     
+     &nbsp;&nbsp;Wir bieten eine <strong>Foto-CD mit &uuml;ber 200 Bildern</strong> an, die Sie entweder <a href="index.php?id=20"><em>hier</em></a> bestellen<br />
+&nbsp;&nbsp;und sich zuschicken lassen oder bei Toni's Kiosk in Illingen erwerben k&ouml;nnen. Wir garantieren<br />
+&nbsp;&nbsp;Ihnen, dass alle Kinder auf den Aufnahmen zu sehen sind. Preis inkl. Versandkosten 7,- &euro;<br />
+&nbsp;&nbsp;(bei Abholung an Toni's Bahnhofskiosk 5,- &euro;).</td>
+       </tr> 
+       <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+--> 
+         
+		<tr>
+			<td height="50px" colspan="3">
+				<hr width="100%" size="1px" noshade style="color:#cccccc; background-color:#CCCCCC;">
+			</td>
+		</tr>
+        <!--
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"> 
+          <strong><font size="+1">Das Jugend-Camp 2012 ist bereits ausgebucht !!!</font></strong><br />
+          Weitere Anmeldungen werden auf die Warteliste gesetzt
+         </p>
+		  </td>
+        </tr>
+        
+        <tr>
+			<td height="50px" colspan="3">
+				<hr width="100%" size="1px" noshade style="color:#cccccc; background-color:#CCCCCC;">
+			</td>
+		</tr>
+    	-->
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong><font size="+1">D</font><font size="+1">er 
+          F&ouml;rderverein SV Illingen-Fu&szlig;ball e.V.<br>
+		  veranstaltet 
+          in den Pfingstferien<br>
+          das Jugend-Fu&szlig;ball-Camp 2015</font></strong></p>
+		  </td>
+        </tr>
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center">vom 
+          27.05.2015 bis 29.05.2015<br>
+          täglich von 9.30 - 16.00 Uhr<br />
+          auf dem Sportgel&auml;nde des SV Illingen (Eichwaldpark).
+          </p>
+		  </td>
+        </tr>
+		<tr id="contentFont">
+          	<td id="contentFont" colspan="3" style="text-align:center; "><p align="center">Teilnehmen 
+          k&ouml;nnen Jugendliche von 5 bis 15 Jahren.</p>
+			</td>
+        </tr>
+<!--
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong>Anmeldung bis sp&auml;testens 01.05.2012 bzw. bis alle Pl&auml;tze belegt sind !!!
+          </strong></p>
+		  </td>
+        </tr>
+-->
+
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="left"> 
+          <strong>Leistungen:</strong><br>
+          3 Tage intensives Training, geleitet von erfahrenen und motivierten Trainern<br>
+          <!--breitensportlich orientiertes DFB &amp; Mc Donald's Fu&szlig;ball-Abzeichen<br>-->
+          Vollbetreuung inkl. Verpflegung (3x Mittagessen, Snacks und Getr&auml;nke &uuml;ber den ganzen Tag)<br>
+          Training in kleinen Gruppen<br>
+          Spezielles Training für Torspieler<br />
+          T-Shirt<br>
+          Urkunde<br>
+          Theoretischer Unterricht<br>
+          Spielerbewertung (freiwillig)<br>
+          Abschlussveranstaltung mit den Eltern </p>
+        </td>
+        </tr>
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="left"> 
+          <strong>Wettbewerbe:</strong><br>
+          Dribbelpark<br>
+          Jonglierk&ouml;nig</p>
+				</td>
+        </tr>
+
+
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="left"> 
+          <strong>Teilnahmegeb&uuml;hr:</strong><br>
+          Einzelanmeldung: &nbsp; &nbsp; &nbsp; 85,00 Euro<br>
+          Geschwister: &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 70,00 Euro/Kind</p>
+		  </td>
+        </tr>
+
+
+		<tr>
+			<td height="50px" colspan="3">
+				<hr width="100%" size="1px" noshade style="color:#cccccc; background-color:#CCCCCC;">
+			</td>
+		</tr>
+  
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong><font size="+1">Die Teilnehmerzahl ist begrenzt !<br />
+Anmeldung bis sp&auml;testens 04.05.2015 an:</font></strong><br />
+        <!--  <strong><font size="+1">Die Anzahl der Teilnehmer ist begrenzt !<br>
+          Anmeldung bis spätestens 01.05.2012 oder bis alle Pl&auml;tze belegt sind an:</font></strong><br /> -->
+          Kontakt / Anmeldung an: W. Conradt, K&auml;the-Kollwitz-Str. 3, 75428 Illingen<br />
+          oder eMail: <a href="mailto:jugendcamp@svi-fussball.de">jugendcamp@svi-fussball.de</a></p>
+		  </td>
+        </tr>
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center">&nbsp; 
+        </p>
+				</td>
+        </tr>
+    
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="left"> 
+          Download: <a href="anmeldung_jugendcamp15.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle">&nbsp;Anmeldeformular 
+          (pdf, 87Kb)</a></p>
+				</td>
+        </tr>
+		<tr id="contentFont">
+          	<td id="contentFont" colspan="3" style="text-align:center; ">
+				<script src="http://connect.facebook.net/de_DE/all.js#xfbml=1"></script>
+                <fb:like href="www.svi-fussball.de/index.php?id=19" send="false" layout="button_count" width="80" show_faces="false" font="verdana"></fb:like>
+			</td>
+        </tr>
+  </table>
+      <p>&nbsp;</p>
+    </td>
