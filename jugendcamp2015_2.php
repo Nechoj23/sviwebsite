de8e551c4f35e134656d5e2c278d48696bee91ae
nechoj <nechoj@googlemail.com>
2015-06-06 22:21:13 +0000

    jugendcamp2015 hinzugefügt

---
+++ jugendcamp2015_2.php
@@ -0,0 +1,102 @@
+<td width="600" align="left" valign="top" bgcolor="#FFFFFF" id="nurlinks">
+      
+	<div style="position:relative; top:-20px; left:0px;  z-index:3 ;width:150px">
+	<table width="600" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td colspan="5" id="contentTable"> 
+            <div align="left">Jugend-Camp 2015</div>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+
+
+		<tr id="contentFont">
+          <td width="91" id="contentFont"><div align="left"><img src="bilder/jugendcamp_logo_k.png" width="100" height="100"></div></td>    
+      <td width="355" id="contentFont"><div align="center"><font size="+2"><strong>
+  Jugend-Camp 2015</strong></font></div></td>
+	  <td width="91" id="contentFont"><div align="left"><img src="bilder/jugendcamp_logo_k.png" width="100" height="100"></div></td>
+        </tr>
+
+<!--         
+     <tr>
+	 <td id="contentfont" colspan="3" style= "text-align:left;"><strong>* <span class="Stil21">Liebe Jugend-Camp-Teilnehmer:</span><span class="Stil21"></span></strong>
+     <p align="center"><a href="index.php?id=20"><img src="bilder/shop/cd02.jpg" width="550" border="0" /></a></p>
+     
+     &nbsp;&nbsp;Wir bieten eine <strong>Foto-CD mit &uuml;ber 200 Bildern</strong> an, die Sie entweder <a href="index.php?id=20"><em>hier</em></a> bestellen<br />
+&nbsp;&nbsp;und sich zuschicken lassen oder bei Toni's Kiosk in Illingen erwerben k&ouml;nnen. Wir garantieren<br />
+&nbsp;&nbsp;Ihnen, dass alle Kinder auf den Aufnahmen zu sehen sind. Preis inkl. Versandkosten 7,- &euro;<br />
+&nbsp;&nbsp;(bei Abholung an Toni's Bahnhofskiosk 5,- &euro;).</td>
+       </tr> 
+       <tr>
+          <td colspan="3">&nbsp;</td>
+        </tr>
+--> 
+         
+		<tr>
+			<td height="50px" colspan="3">
+				<hr width="100%" size="1px" noshade style="color:#cccccc; background-color:#CCCCCC;">
+			</td>
+		</tr>
+        <!--
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"> 
+          <strong><font size="+1">Das Jugend-Camp 2012 ist bereits ausgebucht !!!</font></strong><br />
+          Weitere Anmeldungen werden auf die Warteliste gesetzt
+         </p>
+		  </td>
+        </tr>
+        
+        <tr>
+			<td height="50px" colspan="3">
+				<hr width="100%" size="1px" noshade style="color:#cccccc; background-color:#CCCCCC;">
+			</td>
+		</tr>
+    	-->
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong><font size="+1">R&uuml;ckblick Jugendcamp 2015</font></strong></p>
+		  </td>
+        </tr>
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:left; "><p><br />
+            Vergangene Woche veranstaltete die Fu&szlig;ballabteilung ihr  mittlerweile 8. Jugendcamp im Eichwaldpark. Nachdem am vorletzten Tag noch ein  Kind zu uns stie&szlig;,&nbsp; durften wir dieses  Mal exakt 69 Kinder bzw. Jugendliche bei uns begr&uuml;&szlig;en. Obwohl alle Beteiligten  sp&auml;testens am Freitagabend &bdquo;in den Seilen&ldquo; hingen, hat es auch dieses Mal  wieder einen Riesenspa&szlig; gemacht, diese Veranstaltung bei uns durchzuf&uuml;hren!  Allein die Freude der Kinder in den drei Tagen war es einfach wert. Und ihr,  liebe Kinder, Ihr wart einfach klasse!<br />
+            Wir bedanken uns hiermit bei der Sparkasse Pforzheim-Calw,  bei den Firmen Fontanis und Bonbon-Jung sowie bei Michas Freizeitshop f&uuml;r die  Unterst&uuml;tzung. Dank allen Helfern, ohne die so etwas gar nicht m&ouml;glich w&auml;re.  Die Trainer haben ein absolut kurzweiliges Programm geboten, wobei erw&auml;hnt  werden muss, dass da mancher Urlaubstag geopfert wurde. Auf unsere jugendlichen  Assistenztrainer, die einen Super-Job gemacht haben,&nbsp; k&ouml;nnen wir hoffentlich auch in der Zukunft  bauen (!?). Die K&uuml;che unter Federf&uuml;hrung von Hubert L. Scheuermann hat in  bew&auml;hrter Manier p&uuml;nktlich zu den Essenszeiten geliefert, so dass die Kalorien  anschlie&szlig;end wieder auf dem Sportplatz abgebaut werden konnten.<br />
+            Ein besonderes Dankesch&ouml;n an die Platzwarte Andy und Wolfe  f&uuml;r das herrliche Gel&auml;nde und an Herrn Efthimios Bakas (&bdquo;Thimo&ldquo;), der uns im  Clubhaus &bdquo;wurschteln&ldquo; lie&szlig; und uns als Kr&ouml;nung auch noch eine Geldspende hat zukommen lassen.<br />
+            Irgendwie war es wieder toll!</p><p align="center">
+            <a href="bilder/jugendcamp2015.jpg"><img src="bilder/jugendcamp2015_k.jpg" width="550" alt="Jugend-Camp 2015" align="top"></a>
+            </p>
+		  </td>
+        </tr>
+<!--
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong>Anmeldung bis sp&auml;testens 01.05.2012 bzw. bis alle Pl&auml;tze belegt sind !!!
+          </strong></p>
+		  </td>
+        </tr>
+--><!--
+        <tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center"><strong><font size="+1">Die Teilnehmerzahl ist begrenzt !<br />
+Anmeldung bis sp&auml;testens 14.05.2015 an:</font></strong><br />
+        
+          Kontakt / Anmeldung an: W. Conradt, K&auml;the-Kollwitz-Str. 3, 75428 Illingen<br />
+          oder eMail: <a href="mailto:jugendcamp@svi-fussball.de">jugendcamp@svi-fussball.de</a></p>
+		  </td>
+
+        </tr>
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="center">&nbsp; 
+        </p>
+				</td>
+        </tr>
+    
+		<tr id="contentFont">
+          <td id="contentFont" colspan="3" style="text-align:center; "><p align="left"> 
+          Download: <a href="anmeldung_jugendcamp15.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle">&nbsp;Anmeldeformular 
+          (pdf, 87Kb)</a></p>
+				</td>
+        </tr>
+-->
+  </table>
+      <p>&nbsp;</p>
+    </td>
