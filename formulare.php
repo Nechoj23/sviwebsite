f268e33858de35455e26859e6bb3aa2379a1fad3
nechoj <nechoj@googlemail.com>
2015-07-08 22:15:37 +0000

    Beitrittserklärung vorübergehend auskommentiert

--- formulare.php
+++ formulare.php
@@ -15,15 +15,16 @@
         <tr id="contentFont">
           <td id="contentFont" colspan="2" style="text-align:center; ">
 		  <p align="left">
-
+		<!--
           <a href="eintrittserklaerung_abt_fussball.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle" target="_blank">&nbsp;Eintrittserkl&auml;rung Abteilung Fu&szlig;ball 
           (pdf, 52Kb)</a><br /><br />
+          -->
           <a href="Foerderverein_Beitrittserklaerung.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle" target="_blank">&nbsp;F&ouml;rderverein-Beitrittserkl&auml;rung 
           (pdf, 102Kb)</a><br /><br />
        <!--   <a href="anmeldung_jugendcamp11.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle" target="_blank">&nbsp;Anmeldeformular Jugend-Camp 2011
           (pdf, 69Kb)</a> <br /><br />-->
           <a href="svi-beitragsordnung.pdf"><img src="bilder/adobe_pdf.jpg" width="50" height="50" align="middle" target="_blank">&nbsp;Beitragsordung des SV Illingen
-          (pdf, 22Kb)</a>     </p>		</td>
+          (pdf, 50Kb)</a>     </p>		</td>
         </tr>
 		
 		<tr>
